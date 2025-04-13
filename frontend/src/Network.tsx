10825e93d3bbb2ef5affcb6cb85f8e151e1328a9
prushton2 <peterrushton418@gmail.com>
2025-04-13 17:01:43 -0400

    added status dots

--- frontend/src/Network.tsx
+++ frontend/src/Network.tsx
@@ -1,7 +1,18 @@
+import { JSX } from "react";
 import "./Network.css"
 import { NetworkContainers } from "./models/Network";
 
 export const Network = ({NetworkInfo}: {NetworkInfo: NetworkContainers}) => {
+    let messages: {
+        starting:  [string, string],
+        healthy:   [string, string],
+        unhealthy: [string, string]
+    } = {
+        starting:  ["Service is Starting", "blue"],
+        healthy:   ["Service is Healthy", "green"],
+        unhealthy: ["Service is Unhealthy", "red"]
+    };
+
 
     const formatPorts = (ports: string) => {
         return ports.split(", ").map((port, index) => (
@@ -11,6 +22,20 @@ export const Network = ({NetworkInfo}: {NetworkInfo: NetworkContainers}) => {
             </span>
         ));
     };
+    
+    const getServiceDot = (status: string): JSX.Element => {
+        let health;
+
+        try {
+            health = status.split("(")[1].slice(0,-1);
+        } catch (e) {
+            return <span className="dot" />
+        }
+
+        let healthInfo: [string, string] = messages[health as keyof typeof messages];
+        
+        return <span className={`dot ${healthInfo[1]}`} title={healthInfo[0]}/>
+    }
 
     return <>
         <div className="network-container">
@@ -18,6 +43,7 @@ export const Network = ({NetworkInfo}: {NetworkInfo: NetworkContainers}) => {
             <div className="network-list">
                 {NetworkInfo.Images.map((container, index) => (
                     <div key={index} className="network-item">
+                        {getServiceDot(container.Status)}
                         <h3 className="network-item-element">
                             {container.Names}
                         </h3>
@@ -27,7 +53,6 @@ export const Network = ({NetworkInfo}: {NetworkInfo: NetworkContainers}) => {
                         </p>
                         
                         <p className="network-item-element">
-                            {/* {formatPorts(container.Ports)} */}
                             {NetworkInfo.networkName != "host" ? formatPorts(container.Ports) : <></>}
                         </p>
                     </div>
