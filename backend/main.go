f5162b677478511aafe5579ee2743c0378d4821a
prushton2 <peterrushton418@gmail.com>
2025-04-12 20:16:25 -0400

    added backend

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"io"
+	"os/exec"
+	"strings"
+)
+
+func getContainerInfo(w http.ResponseWriter, r *http.Request) {
+	cmd := exec.Command("docker", "ps","--format","'{{json .}}'")
+	out, err := cmd.Output()
+	if(err != nil) {
+		fmt.Println(err)
+	}
+	str := strings.Replace(string(out), "\\\"", "", -1)
+	str = str[1 : len(str)-2]
+	str = "[" + str + "]"
+
+	io.WriteString(w, str)
+}
 
 func main() {
-	fmt.Println("aaa")
+	http.HandleFunc("/containerInfo", getContainerInfo)
+
+	_ = http.ListenAndServe(":3000", nil)
 }
