361b5020242931c57beffcd241fbb0319ab4aa0c
prushton2 <peterrushton418@gmail.com>
2025-04-12 21:26:25 -0400

    uses env variables

--- frontend/src/API.tsx
+++ frontend/src/API.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getContainerInfo = async (): Promise<string> => {
     try {
-        const response = await axios.get('http://localhost:3000/containerInfo');
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/containerInfo`);
         return JSON.stringify(response.data);
     } catch (error) {
         console.error('Error fetching container info:', error);
