d60fea870d1d47881342bafe3415fec36c7aec2b
prushton2 <peterrushton418@gmail.com>
2026-02-21 16:24:16 -0500

    updated text

--- frontend/src/Components/Ribbon.tsx
+++ frontend/src/Components/Ribbon.tsx
@@ -6,11 +6,11 @@ function Ribbon({healthSummary}: {healthSummary: HealthSummary}) {
     return (<>
         <div 
             className={`Summary ${healthSummary.unhealthy == 0 ? "Success" : "Fail"}`} 
-            title={healthSummary.unhealthy == 0 ? "All Services Running" : `${healthSummary.unhealthy} Service${healthSummary.unhealthy == 1 ? " is" : "s are"} Unhealthy`}>
+            title={healthSummary.unhealthy == 0 ? "All Services Healthy" : `${healthSummary.unhealthy} Service${healthSummary.unhealthy == 1 ? " is" : "s are"} Unhealthy`}>
             {healthSummary.unhealthy == 0 ? "✓" : "X"}
         </div>
         <div className="Ribbon">
-            linode.prushton.com
+            prushton.com
         </div>
     </>);
 }
